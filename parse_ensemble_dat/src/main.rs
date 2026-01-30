e3ab35660d41a333f0b48a0849ef07179c5afca4
Nick Carter <npcarter@gmail.com>
2026-01-30 17:44:58 -0500

    progress on parsing Ensembl

--- parse_ensemble_dat/src/main.rs
+++ parse_ensemble_dat/src/main.rs
@@ -13,6 +13,7 @@ struct Cli {
 struct exon{
     start: i64,
     end: i64,
+    complement: bool,
 }
 
 struct intron{
@@ -20,14 +21,19 @@ struct intron{
     end: i64,
 }
 
+struct protein{
+    introns: Vec<intron>,
+    exons: Vec<exon>,
+    dna_sequence: Vec<char>,
+    protein_sequence: Vec<char>,
+}
 struct gene{
     start: i64, // start position of the gene in the contig
     end: i64, // end position of the gene in the contig.
     // gene runs from start to end, inclusive
+    name: String,
     dna_sequence: Vec<char>,
-    protein_sequence: Vec<char>,
-    exons: Vec<exon>,
-    introns: Vec<intron>,
+    proteins: Vec<protein>,
 }
 struct contig{
     length: i64,
@@ -43,35 +49,52 @@ fn process_contig(contig: &contig){
 
 fn parse_sequence(fields: Vec<&str>, lines: &mut Peekable<Lines<BufReader<File>>>) -> Vec<char>{
     let mut sequence:Vec<char> = Vec::new();
-    let length:i64 = fields[1].parse::<i64>().unwrap();
+    let length:i64 = fields[2].parse::<i64>().unwrap();
     let mut residue_count:i64 = 0;
-    while (&mut *lines).peek().unwrap().unwrap() != "//".to_string(){
+    let mut next_line = match lines.peek(){
+        Some(Ok(line)) => line.to_string(),
+        _ => panic!("Unexpected end of file")
+    };
+    while next_line != "//".to_string(){
         let line = (&mut *lines).next().unwrap().unwrap();
         let fields = line.split_whitespace().collect::<Vec<&str>>();
-        let end:i64 = fields[-1].parse::<i64>().unwrap();
-        for residues in fields[0..-1].iter(){
+        let end:i64 = fields.last().unwrap().parse::<i64>().unwrap();
+        for residues in fields[0..(fields.len()-1)].iter(){
             for residue in residues.chars() {
                 sequence.push(residue);
                 residue_count += 1;
             }
         }
         if residue_count != end{println!("Error: residue count {} does not match end position {}", residue_count, end);}
+        next_line = match lines.peek(){
+            Some(Ok(line)) => line.to_string(),
+            _ => panic!("Unexpected end of file")
+        };
     }
-
+    println!("Sequence length is {}", sequence.len());
     sequence
 }
 
 fn parse_gene(fields: Vec<&str>, lines: &mut Peekable<Lines<BufReader<File>>>) -> gene{
-    let mut the_gene = gene{start:0, end:0, dna_sequence: Vec::new(), protein_sequence: Vec::new(), exons: Vec::new(),
-    introns: Vec::new()};
-    let mut next_line:String = (&mut *lines).peek().unwrap().unwrap();
+    let mut the_gene = gene{start:0, end:0, dna_sequence: Vec::new(), proteins: Vec::new(), name: "".to_string()};
+
+
+    let mut next_line = match lines.peek(){
+        Some(Ok(line)) => line.to_string(),
+        _ => panic!("Unexpected end of file")
+    };
     let mut next_fields = next_line.split_whitespace().collect::<Vec<&str>>();
     while next_fields[0] != "XX" && next_fields[0] != "SQ" && next_fields[0] != "//" &&
         !(next_fields[0] == "FT" && next_fields[1] == "gene"){
         // We're still in the gene
 
+        let line = (&mut *lines).next().unwrap().unwrap();
+        let fields = line.split_whitespace().collect::<Vec<&str>>();
 
-        next_line = (&mut *lines).peek().unwrap().unwrap();
+        next_line = match lines.peek(){
+            Some(Ok(line)) => line.to_string(),
+            _ => panic!("Unexpected end of file")
+        };
         next_fields = next_line.split_whitespace().collect::<Vec<&str>>();
     }
     the_gene
@@ -87,26 +110,46 @@ fn main() {
         organism: "".to_string(),
         name: "".to_string()};
     let mut lines:Peekable<Lines<BufReader<File>>> = reader.lines().peekable();
-    while let Some(Ok(line)) = lines.next() { // there's still data in the file
+    let mut gene_count:i64 = 0;
+    while let Some(line_result) = lines.next(){ // there's still data in the file
+        let line = line_result.unwrap();
         let fields = line.split_whitespace().collect::<Vec<&str>>();
-        match fields[0]{
+        match fields[0] {
             "ID" => {
+                println! {"Processing contig {}", fields[1]};
+                if contig.name != "" { process_contig(&contig); }
+                contig = contig { dna_sequence: Vec::new(), genes: Vec::new(), length: 0, organism: "".to_string(), name: "".to_string() };
                 contig.name = fields[1].to_string();
                 contig.length = fields[5].parse::<i64>().unwrap();
-                break;
+                println!("contig length is {}", contig.length);
             },
-            "OS" => {contig.organism = fields[1..].join(" ").to_string(); break;},
+            "OS" => {
+                contig.organism = fields[1..].join(" ").to_string();
+            },
             "FT" => {
-                if fields[1] == "gene"{contig.genes.push(parse_gene(fields[2..], &lines))}
-                else{println!("Unexpected FT line: {}", fields.join(" "))}
-                break;
+
+                if fields[1] == "gene" {
+                    gene_count += 1;
+                    contig.genes.push(parse_gene(fields, &mut lines))
+                } else {};
             }
-            "SQ" =>{contig.dna_sequence = parse_sequence(fields, &mut lines); break;},
-            "//" => {process_contig(&contig); break;},
-            _ => println!("Unexpected line: {}", line)
+            "SQ" => {
+                contig.dna_sequence = parse_sequence(fields, &mut lines);
+            },
+            "//" => {
+                process_contig(&contig);
+            },
+            "XX" => {}, // spacer line
+            "AC" => {}, // accession line, we don't care about this
+            "SV" => {}, // sequence version line, we don't care about this
+            "DT" => {}, // date line, we don't care about this
+            "DE" => {}, // description line, we don't care about this
+            "KW" => {}, // keywords line, we don't care about this
+            "OC" => {}, // organism classification line, we don't care about this
+            "CC" => {}, // comment line, we don't care about this
+            "FH" => {}, // feature header line, we don't care about this
+            _ => println!("Unexpected line: {}", line),
         }
-
-
     }
-
+    println!("Processed {} genes", gene_count);
 }
